3275f1890542586ae8c91f97102a922e027d074e
ezotaka <34199555+ezotaka@users.noreply.github.com>
2022-08-18 15:29:05 +0000

    gorun: use 'go test' command

--- gorun/gorun_test.go
+++ gorun/gorun_test.go
@@ -21,11 +21,11 @@ package gorun
 
 import (
 	"bytes"
+	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
-
-	"github.com/ezotaka/golib/ezast"
 )
 
 func getStdout(t *testing.T, fn func()) string {
@@ -49,6 +49,18 @@ func getStdout(t *testing.T, fn func()) string {
 }
 
 func TestExec(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	goModFile, err := findTowardRoot(".", "go.mod")
+	if err != nil {
+		t.Fatal(err)
+	}
+	goModDir := filepath.Dir(goModFile)
+	const outOfGoModDir = "/this/file/maybe/out/of/go/module/dir/59432543040540/src1.go"
+
 	type args struct {
 		file string
 		fn   string
@@ -63,6 +75,14 @@ func TestExec(t *testing.T) {
 			name: "Run func test1 in package testdata",
 			args: args{
 				file: "testdata/src1.go",
+				fn:   "test1",
+			},
+			want: "test1",
+		},
+		{
+			name: "absolute file path",
+			args: args{
+				file: filepath.Join(wd, "testdata/src1.go"),
 				fn:   "test1",
 			},
 			want: "test1",
@@ -86,6 +106,15 @@ func TestExec(t *testing.T) {
 			wantErr: true,
 		},
 		{
+			name: "file is out of current go module dir",
+			args: args{
+				file: outOfGoModDir,
+				fn:   "test1",
+			},
+			want:    fmt.Sprintf("file '%s' must be in go module dir '%s'", outOfGoModDir, goModDir),
+			wantErr: true,
+		},
+		{
 			name: "file is empty",
 			args: args{
 				file: "",
@@ -105,9 +134,9 @@ func TestExec(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
+			// DO NOT t.Parallel()
+			//t.Parallel()
 
 			var err error
 			goRun := func() {
@@ -126,37 +155,122 @@ func TestExec(t *testing.T) {
 					t.Errorf("Exec() = %v, want %v", got, tt.want)
 				}
 			}
+
+			// Exec() changes working directory temporarily,
+			// so check that current directory is not changed
+			doneWd, err := os.Getwd()
+			if err != nil {
+				t.Fatal(err)
+			} else if doneWd != wd {
+				t.Errorf("Exec() doesn't clean up. got wd = %s, want wd = %s", doneWd, wd)
+			}
 		})
 	}
 }
 
-func Test_convert(t *testing.T) {
+func Test_findTowardRoot(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	abs := func(path string) string {
+		return filepath.Join(wd, path)
+	}
+
 	type args struct {
-		file string
-		fn   string
+		baseDir string
+		file    string
 	}
 	tests := []struct {
-		name string
-		args args
-		want string
+		name    string
+		args    args
+		want    string
+		wantErr bool
 	}{
 		{
-			name: "change package name to main, and swap test1 func and main func",
+			name: "found in absolute dir path",
 			args: args{
-				file: "testdata/src1.go",
-				fn:   "test1",
+				baseDir: abs("testdata/a/b/c"),
+				file:    "d.txt",
 			},
-			want: "testdata/ans1.go",
+			want: abs("testdata/a/b/c/d.txt"),
+		},
+		{
+			name: "found in relative dir path",
+			args: args{
+				baseDir: "testdata/a/b/c",
+				file:    "d.txt",
+			},
+			want: abs("testdata/a/b/c/d.txt"),
+		},
+		{
+			name: "found in parent of absolute dir path",
+			args: args{
+				baseDir: abs("testdata/a/b/c"),
+				file:    "b.txt",
+			},
+			want: abs("testdata/a/b.txt"),
+		},
+		{
+			name: "found in parent of relative dir path",
+			args: args{
+				baseDir: "testdata/a/b/c",
+				file:    "b.txt",
+			},
+			want: abs("testdata/a/b.txt"),
+		},
+		{
+			name: "dir with matching name are ignored",
+			args: args{
+				baseDir: "testdata/a/b/c",
+				file:    "b",
+			},
+			want: abs("testdata/b"),
+		},
+		{
+			name: "file not found",
+			args: args{
+				baseDir: "testdata/a/b/c",
+				file:    "this_file_is_maybe_not_found_78438598435984324532",
+			},
+			wantErr: true,
+		},
+		{
+			name: "baseDir not found",
+			args: args{
+				baseDir: "testdata/a/b/c/notFound",
+				file:    "b.txt",
+			},
+			wantErr: true,
+		},
+		{
+			name: "baseDir is empty",
+			args: args{
+				baseDir: "",
+				file:    "b.txt",
+			},
+			wantErr: true,
+		},
+		{
+			name: "file is empty",
+			args: args{
+				baseDir: "testdata/a/b/c",
+				file:    "",
+			},
+			wantErr: true,
 		},
 	}
+
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			got, _ := convert(tt.args.file, tt.args.fn)
-			want, _ := ezast.NewAstFile(tt.want)
-			if !ezast.AssertEqualAstFile(got.File, want.File) {
-				t.Errorf("convert() = %v, want %v", got.String(), want.String())
+			got, err := findTowardRoot(tt.args.baseDir, tt.args.file)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("findTowardRoot() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("findTowardRoot() = %v, want %v", got, tt.want)
 			}
 		})
 	}
